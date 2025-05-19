0119862712abd5879d28325b4c0a5756af26ef37
xXmarcosX <marcos.v3567@gmail.com>
2025-05-19 05:27:45 -0300

    Adição do componente counter

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,11 @@
+import styles from './Counter.module.css'
+
+export default function Counter() {
+  return (
+    <>
+      <div className={styles.counterContainer}>
+        <p>00:00</p>
+      </div>
+    </>
+  )
+}
