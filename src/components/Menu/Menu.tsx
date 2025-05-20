92d61536064340ef616aa2d42c9eac978e44ae28
xXmarcosX <marcos.v3567@gmail.com>
2025-05-20 19:27:47 -0300

    merge

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,27 +1,51 @@
 import { History, Home, Settings, Sun } from 'lucide-react'
 import styles from './Menu.module.css'
 import MenuIcon from './MenuIcon/MenuIcon'
+import { useState, useEffect } from 'react'
+
+type AvailableThemes = 'light' | 'dark'
 
 export default function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark')
+
+  const handleThemeChange = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    e.preventDefault()
+    
+    theme === 'dark' ? setTheme('light') : setTheme('dark')
+
+    document.documentElement.setAttribute('data-theme', theme)
+  }
+
   const btns = [
     {
-      icon: <Home/>
+      icon: <Home />,
+      label: 'Ir para home',
     },
     {
-      icon: <History/>
+      icon: <History />,
+      label: 'Ir para histórico'
     },
     {
-      icon: <Settings/>
+      icon: <Settings />,
+      label: 'Ir para configurações'
     },
     {
-        icon: <Sun/>
+      icon: <Sun />,
+      label: 'Alterar tema',
+      funcao: handleThemeChange
     }
   ]
 
   return (
     <>
       <nav className={styles.container}>
-        {btns.map(bt => <MenuIcon>{bt.icon}</MenuIcon>)}
+        {btns.map(bt =>
+          <MenuIcon
+            key={bt.label}
+            label={bt.label}
+            func={bt.funcao && bt.funcao}>
+            {bt.icon}
+          </MenuIcon>)}
       </nav>
     </>
   )
--- src/components/Menu/MenuIcon/MenuIcon.tsx
+++ src/components/Menu/MenuIcon/MenuIcon.tsx
@@ -1,14 +1,16 @@
 import styles from './MenuIcon.module.css'
 
 type MenuIconProps = {
-  children: React.ReactNode
+  children: React.ReactNode,
+  label: string,
+  func?: (e: any) => void
 }
 
-export default function MenuIcon({children}: MenuIconProps) {
+export default function MenuIcon({children, label, func}: MenuIconProps) {
   return (
     <>
-      <a href='' className={styles.link}>
-        <button className={styles.button}>{children}</button>
+      <a href='' className={styles.link} aria-label={label} title={label}>
+        <button className={styles.button} onClick={func}>{children}</button>
       </a>
     </>
   )
