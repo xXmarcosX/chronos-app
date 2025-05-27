8d6f0ac81e4af2a4c6079e4bb15974ec1e7ec5c4
2830482511008 <2830482511008@fatecsdp.acad.local>
2025-05-27 14:06:58 -0300

    Validação do formulário e criação da nova tarefa

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -4,13 +4,45 @@ import Cycles from "../Cycles/Cycles"
 
 import { PlayCircleIcon } from "lucide-react"
 import { useState } from "react"
+import type { TaskModel } from "../../models/TaskModel"
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 
 export default function MainForm() {
+  const {setState} = useTaskContext()
 
   const [taskName, setTaskName] = useState('')
 
   const handleStartNewTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    if (!taskName.trim()) {
+      alert('Digite o nome da tarefa')
+      return
+    } 
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'worktime'
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {...prevState.config},
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +52,6 @@ export default function MainForm() {
   return (
     <>
       <form action="" className='form' onSubmit={handleStartNewTask}>
-        <h1>form foi enviado vezes</h1>
         <div className="formRow">
           <Input
             type='text'
@@ -34,7 +65,7 @@ export default function MainForm() {
         </div>
 
         <div className="formRow">
-          <p>O próximo intervaloe é de 25 minutos</p>
+          <p>O próximo intervalo é de 25 minutos</p>
         </div>
 
         <div className="formRow">
